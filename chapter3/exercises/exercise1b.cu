a4889b4983bc57069eeb0261b05a7e3f4d788b4f
lucaslingle <lucasdaxlingle@gmail.com>
2024-02-17 20:05:56 -0800

    commit

--- chapter3/exercises/exercise1b.cu
+++ chapter3/exercises/exercise1b.cu
@@ -1,6 +1,6 @@
 /* Write a kernel that has one thread to produce each output matrix col */
 
-__global__ void MatmulRowthreadKernel(float *M, float *N, float *P, int imax, int kmax, int jmax)
+__global__ void MatmulColthreadKernel(float *M, float *N, float *P, int imax, int kmax, int jmax)
 {
     /* ik, kj -> ij */
     int j = blockIdx.x * blockDim.x + threadIdx.x;
@@ -20,8 +20,26 @@ __global__ void MatmulRowthreadKernel(float *M, float *N, float *P, int imax, in
 
 float *MatmulColthreadStub(float *M, float *N, float *P, int imax, int kmax, int jmax)
 {
+    int msize = imax * kmax * sizeof(float);
+    int nsize = kmax * jmax * sizeof(float);
+    int psize = imax * jmax * sizeof(float);
+    float *M_d, *N_d, *P_d;
+
+    cudaMalloc((void **)&M_d, msize);
+    cudaMalloc((void **)&N_d, nsize);
+    cudaMalloc((void **)&P_d, psize);
+
+    cudaMemcpy(M_d, M, msize, cudaMemcpyHostToDevice);
+    cudaMemcpy(N_d, N, nsize, cudaMemcpyHostToDevice);
+
     int thread_block_size = 256;
     dim3 dimsGrid(ceil(imax / thread_block_size), 1, 1);
     dim3 dimsBlock(thread_block_size, 1, 1);
-    MatmulRowthreadKernel<<<dimsGrid, dimsBlock>>>(M, N, P, imax, kmax, jmax);
+    MatmulColthreadKernel<<<dimsGrid, dimsBlock>>>(M_d, N_d, P_d, imax, kmax, jmax);
+
+    cudaMemcpy(P, P_d, psize, cudaMemcpyDeviceToHost);
+
+    cudaFree(M_d);
+    cudaFree(N_d);
+    cudaFree(P_d);
 }
