686604ab98de2f1a6ce57607370ad1aa8a871bc3
lucaslingle <lucasdaxlingle@gmail.com>
2024-03-10 23:30:10 -0700

    commit tiled sq matmul with random test

--- chapter5/tiled_square_matmul.cu
+++ chapter5/tiled_square_matmul.cu
@@ -1,6 +1,10 @@
-#define TILE_WIDTH 16
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-__global__ void matmulKernel(float *M, float *N, float *P, int Width)
+#define TILE_WIDTH 2
+
+__global__ void tiledMatmulKernel(float *M, float *N, float *P, int Width)
 {
     __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
     __shared__ float Nds[TILE_WIDTH][TILE_WIDTH];
@@ -27,4 +31,77 @@ __global__ void matmulKernel(float *M, float *N, float *P, int Width)
         __syncthreads();
     }
     P[row * Width + col] = pvalue;
+}
+
+void tiledMatmulStub(float *M, float *N, float *P, int Width)
+{
+    int MatBytes = Width * Width * sizeof(float);
+    float *M_d, *N_d, *P_d;
+
+    cudaMalloc((void **)&M_d, MatBytes);
+    cudaMalloc((void **)&N_d, MatBytes);
+    cudaMalloc((void **)&P_d, MatBytes);
+    cudaMemcpy(M_d, M, MatBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(N_d, N, MatBytes, cudaMemcpyHostToDevice);
+
+    int n_tiles = (Width + TILE_WIDTH - 1) / TILE_WIDTH;
+    dim3 dimsGrid(n_tiles, n_tiles, 1);
+    dim3 dimsBlock(TILE_WIDTH, TILE_WIDTH, 1);
+    tiledMatmulKernel<<<dimsGrid, dimsBlock>>>(M_d, N_d, P_d, Width);
+
+    cudaMemcpy(P, P_d, MatBytes, cudaMemcpyDeviceToHost);
+    cudaFree(M_d);
+    cudaFree(N_d);
+    cudaFree(P_d);
+}
+
+int main()
+{
+    float *M, *N, *P, *P_Expected;
+    int Width = 4;
+    int MatBytes = Width * Width * sizeof(float);
+
+    M = (float *)malloc(MatBytes);
+    N = (float *)malloc(MatBytes);
+    P = (float *)malloc(MatBytes);
+    P_Expected = (float *)malloc(MatBytes);
+
+    srand(time(NULL));
+    for (int i = 0; i < Width; ++i)
+    {
+        for (int j = 0; j < Width; ++j)
+        {
+            M[i * Width + j] = (float)(rand() % 100);
+            N[i * Width + j] = (float)(rand() % 100);
+        }
+    }
+
+    for (int i = 0; i < Width; ++i)
+    {
+        for (int j = 0; j < Width; ++j)
+        {
+            for (int k = 0; k < Width; ++k)
+            {
+                P_Expected[i * Width + j] += M[i * Width + k] * N[k * Width + j];
+            }
+        }
+    }
+
+    tiledMatmulStub(M, N, P, Width);
+    for (int i = 0; i < Width; ++i)
+    {
+        for (int j = 0; j < Width; ++j)
+        {
+            /* printf("P_%d,%d: %0.1f\n", i, j, P[i * Width + j]); */
+            if (P[i * Width + j] != P_Expected[i * Width + j])
+            {
+                printf("Value P[%d,%d] was %0.1f, expected %0.1f\n", i, j, P[i * Width + j], 2 * M[i * Width + j]);
+            }
+        }
+    }
+
+    free(M);
+    free(N);
+    free(P);
+    return 0;
 }
